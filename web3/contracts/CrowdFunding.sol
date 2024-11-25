df80d27de4f203cdcda15d45532c3f8f13a6ce40
fahad39 <fahadhussain0127@gmail.com>
2024-11-25 23:13:33 +0500

    added code

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract CrowdFunding {
+    struct Campaign{
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uinit256 deadline;
+        uinit256 amountCollected;
+        string image;
+        address[] donators;
+        uinit256[] donations;
+    }
+
+    mapping(uinit256 => Campaign) public campaign;
+
+    uinit256 public numberofcampaigns = 5;
+
+    function createCompaigns(){}
+
+    function donateToCampaign(){}
 }
